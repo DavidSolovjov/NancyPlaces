72bf3dd29d6c136ab24f5bc7e8aed1281883cad6
Rasselio Abou <rasselio.diack@gmail.com>
2017-07-07 18:33:45 +0200

    Setting for the first times our different files

---
+++ js/myapp.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+  google.maps.event.addDomListener(window, 'load', initMap);
+
+  function initMap() {
+    
+
+
+}
+
+
+
+  // Here we are going to implement functionalities with knockoutjs
+
+
+});
